153a2f5a6c59f8ea0472b087cb7c16d766980f25
Rusvelt Jose Meza San Martin <rmezas@unal.edu.co>
2023-09-18 18:20:29 -0500

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/pooej18/POOEJ18.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooej18;
+
+/**
+ *
+ * @author rusve
+ */
+public class POOEJ18 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
